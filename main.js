628049c224d45d4aefb49132c68f5093592bdb44
MdMahbub-h <2021339001@student.sust.edu>
2024-11-10 03:46:02 +0600

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const config = {
     default: "arcade",
     arcade: {
       gravity: { y: 0 },
-      debug: true,
+      debug: false,
     },
   },
   scene: [WelcomeScene, StartScene, GameScene, WinScene, LoseScene],
